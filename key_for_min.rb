86372f25b3aebe09bed30ba236581d323b0e4984
Sean Good <tree662@protonmail.com>
2019-11-14 19:39:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  # name_a = name_hash.to_a
+  name_a = name_hash.to_a
 
 
-  # value= 1000
-  # key= 0
+  value= 1000
+  key= 0
 
   name_a.collect do |key, value|
       if value < name_a[key]
-        # default_value = i[1]
-        # default_key = i[0]
+        default_value = i[1]
+        default_key = i[0]
         return default_key
       else
         return nil
