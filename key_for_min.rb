7e3b52d33d84af53d2b17607621bd800e7d81b64
Sean Good <tree662@protonmail.com>
2019-11-14 19:38:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,13 +5,13 @@ def key_for_min_value(name_hash)
   name_a = name_hash.to_a
 
 
-  default_value= 1000
-  default_key= 0
+  value= 1000
+  key= 0
 
-  name_a.collect do |i|
-      if i[1] < default_value
-        default_value = i[1]
-        default_key = i[0]
+  name_a.collect do |key, value|
+      if value < name_a[key]
+        # default_value = i[1]
+        # default_key = i[0]
         return default_key
       else
         return nil
